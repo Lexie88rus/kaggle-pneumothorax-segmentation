fce27cda8b927894fb21ca91f5111408b386879d
Aleksandra Astakhova <lexie@live.ru>
2019-07-25 18:08:30 +0300

    Added deep supervision to UNet++ nested model.

--- models/unet_plusplus.py
+++ models/unet_plusplus.py
@@ -113,6 +113,7 @@ class NestedUNet(nn.Module):
         else:
             self.final = nn.Conv2d(nb_filter[0], 1, kernel_size=1)
 
+        self.fuse = FusionLayer()
 
     def forward(self, input):
         x0_0 = self.conv0_0(input)
@@ -139,8 +140,27 @@ class NestedUNet(nn.Module):
             output2 = self.final2(x0_2)
             output3 = self.final3(x0_3)
             output4 = self.final4(x0_4)
-            return [output1, output2, output3, output4]
+            output = self.fuse([output1, output2, output3, output4])
+
+            return output
 
         else:
             output = self.final(x0_4)
             return output
+
+    # fusion features
+    # https://github.com/AceCoooool/DSS-pytorch/blob/master/dssnet.py
+    class FusionLayer(nn.Module):
+        def __init__(self, nums=4):
+            super(FusionLayer, self).__init__()
+            self.weights = nn.Parameter(torch.randn(nums))
+            self.nums = nums
+            self._reset_parameters()
+
+        def _reset_parameters(self):
+            init.constant_(self.weights, 1 / self.nums)
+
+        def forward(self, x):
+            for i in range(self.nums):
+                out = self.weights[i] * x[i] if i == 0 else out + self.weights[i] * x[i]
+            return out
