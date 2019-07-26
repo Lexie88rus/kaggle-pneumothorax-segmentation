b7b681cde84873e9ebcac25a93ccd928ae57fa92
Aleksandra Astakhova <lexie@live.ru>
2019-07-26 16:22:22 +0300

    Added Jaccard loss implementation.

---
+++ jaccard_loss.py
@@ -0,0 +1,54 @@
+'''
+Script for Jaccard loss (IOU loss)
+Source:
+https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
+'''
+import torch
+import torch
+from torch.nn import functional as F
+from torch import nn
+
+def jaccard_loss(true, logits, eps=1e-7):
+    """Computes the Jaccard loss, a.k.a the IoU loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the jaccard loss so we
+    return the negated jaccard loss.
+    Args:
+        true: a tensor of shape [B, H, W] or [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        jacc_loss: the Jaccard loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(probas, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    union = cardinality - intersection
+    jacc_loss = (intersection / (union + eps)).mean()
+    return (1 - jacc_loss)
+
+class JaccardLoss(nn.Module):
+    '''
+    Jaccard (IoU) loss
+    '''
+    def __init__(self):
+        super(JaccardLoss, self).__init__()
+
+    def forward(self, input, target):
+        return jaccard_loss(target, input)
